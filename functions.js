afa40a412b586d558ad7687625d15733892285df
Hedlundaren <hedlundaren93@hotmail.com>
2015-12-24 03:51:41 +0100

    INIT

---
+++ functions.js
@@ -0,0 +1,34 @@
+
+
+var check = 0;
+
+$(document).ready(function(){
+$("h1").hover(
+function() {
+$("h1").stop().animate({"opacity": "0.5"}, "medium");
+});
+
+});
+
+
+$(document).ready(function(){
+	//$('body').click(
+
+	//	function() {
+			if(!check){
+				check = true;
+	    $({blurRadius: 40}).animate({blurRadius: 0}, {
+	        duration: 3000,
+	        easing: 'swing', // or "linear"
+	                         // use jQuery UI or Easing plugin for more options
+	        step: function() {
+	            $('#background').css({ "filter": "grayscale(100%) blur("+this.blurRadius+"px)"  }, "medium");
+	        }
+	    });
+	}
+	//});
+
+});	
+
+			
+
